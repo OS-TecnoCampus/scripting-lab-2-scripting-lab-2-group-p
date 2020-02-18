5eb2e248bafdff49e7e3446f2c9890196b005dc7
Joel <jramosc@edu.tecnocampus.cat>
2020-02-18 13:03:46 +0100

    Cover done

--- PDFGenerator.py
+++ PDFGenerator.py
@@ -11,10 +11,35 @@ pdf.set_line_width(4)
 pdf.set_draw_color(255, 200, 0)
 pdf.line(30, 275, 30, 10)
 
-#setting up the cover image
+# setting up the cover image
 script_dir = os.path.dirname(__file__)
 image_path = "../resources/tecnocampus.png"
 file_path = os.path.join(script_dir, image_path)
 pdf.image("C:/Users/Joel/PycharmProjects/scripting-lab-2-scripting-lab-2-group-p/resources/tecnocampus.png", x=140, y=50, w=60) # MIRAR
+
+# setting up the rest of the cover
+f = open("C:/Users/Joel/PycharmProjects/scripting-lab-2-scripting-lab-2-group-p/resources/cover.txt", "r") # MIRAR
+if f.mode == "r":
+    lines = f.readlines()
+    counter = 1
+    for line in lines:
+        if counter == 1:
+            pdf.set_font_size(24)
+            pdf.text(40, 150, line)
+        elif counter == 2:
+            pdf.text(40, 170, line)
+        elif counter == 3:
+            pdf.set_font_size(12)
+            pdf.text(40, 200, line)
+        elif counter == 4:                  # estos tres if quizas podrian hacerse mejor con un cell
+            pdf.set_font("Arial", 'B', 8)
+            pdf.text(40, 254, line)
+        elif counter == 5:
+            pdf.text(0, 258, line)
+        else:
+            pdf.text(0, 262, line)
+        counter = counter + 1
+
+# Tengo que mirar bien como coger una path porque lo que he encontrado no ha funcionado
 
 pdf.output('test.pdf')
