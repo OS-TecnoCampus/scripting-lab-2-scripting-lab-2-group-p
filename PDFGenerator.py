04d82f9bac7e4ee1c5611e4405b590ee40ed5685
Joel <jramosc@edu.tecnocampus.cat>
2020-02-19 12:27:46 +0100

    Pequeño arreglo

--- PDFGenerator.py
+++ PDFGenerator.py
@@ -1,5 +1,4 @@
 from fpdf import FPDF
-import os
 
 # setting up the pdf configuration
 pdf = FPDF(orientation='P', unit='mm', format='A4')
@@ -12,13 +11,10 @@ pdf.set_draw_color(255, 200, 0)
 pdf.line(30, 275, 30, 10)
 
 # setting up the cover image
-script_dir = os.path.dirname(__file__)
-image_path = "../resources/tecnocampus.png"
-file_path = os.path.join(script_dir, image_path)
-pdf.image("C:/Users/Joel/PycharmProjects/scripting-lab-2-scripting-lab-2-group-p/resources/tecnocampus.png", x=140, y=50, w=60) # MIRAR
+pdf.image("resources/tecnocampus.png", x=140, y=50, w=60) # MIRAR
 
 # setting up the rest of the cover
-f = open("C:/Users/Joel/PycharmProjects/scripting-lab-2-scripting-lab-2-group-p/resources/cover.txt", "r") # MIRAR
+f = open("resources/cover.txt", "r") # MIRAR
 if f.mode == "r":
     lines = f.readlines()
     counter = 1
@@ -41,5 +37,7 @@ if f.mode == "r":
         counter = counter + 1
 
 # Tengo que mirar bien como coger una path porque lo que he encontrado no ha funcionado
+
+
 
 pdf.output('test.pdf')
